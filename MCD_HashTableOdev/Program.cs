c504da7b3b2e374923a80c46b5e95c1893f9cdb1
Faik Turan <turanfaik@hotmail.com>
2021-11-22 21:02:47 +0300

    Add project files.

---
+++ MCD_HashTableOdev/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_HashTableOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hashtable içerisine ENG  - TR olarak data eklemeniz. 
+            /*
+             * Yeni kayıt eklemek istiyormusunuz E/H
+             * E : Yeni kayıt ekleme işlemi devam etsin, 
+             * H : Tüm listeyi yazdırın. 
+             * 
+             * Var olan bir key değeri ekliyor ise kullanıcıya bu değer daha önceden ... şeklinde sistemimizde bulunmaktadır. 
+             * 
+             */
+            //DictionaryEntry
+        }
+    }
+}
